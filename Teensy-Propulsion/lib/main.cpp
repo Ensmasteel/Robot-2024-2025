9e4e83ae8c0649f6b6c1e4b3fd069d40c3b096f5
xXxFetraxXx <alexandre51160@gmail.com>
2024-09-30 09:43:35 +0200

    Numeriexplore

--- Teensy-Propulsion/lib/main.cpp
+++ Teensy-Propulsion/lib/main.cpp
@@ -19,6 +19,7 @@ SequenceManager* brain;
 SequenceManager* brain_bleu;
 SequenceManager* brain_jaune;
 SequenceManager* brain_numeriexplore;
+SequenceManager* brain_numeriexplore2;
 
 Threads::Mutex tirrette_mut;
 
@@ -299,7 +300,29 @@ void setup() {
             new StaticAction(SOLAR_LEFT_OFF,true)
         }
     );
-    brain_numeriexplore = new SequenceManager({Brain_Numeriexplore}); 
+    Sequence Brain_Numeriexplore2(
+        {
+            new StaticAction(OPEN_CLAWS,true),
+            new StaticAction(LOWER_CLAWS,true),
+
+            new StaticAction(START_MAGNET,true),
+            new MoveAction(VectorOriented( -0.30f, 0.0f,  0.0f), false, true, true, true),
+            new MoveAction(VectorOriented( 0.0f,  0.0f,  0.0f), false, false, true, true),
+            new MoveAction(VectorOriented( 0.30f,  0.0f,  0.0f), false, false, true, true),
+            new StaticAction(CLOSE_CLAWS),
+            new MoveAction(VectorOriented( 0.0f,  0.0f,  0.0f), false, true, true, true),
+            new StaticAction(SHUTDOWN_MAGNET,true),
+            new StaticAction(RAISE_CLAWS),
+            new MoveAction(VectorOriented( 0.13f,  0.0f,  0.0f), false, false, true, true),
+            new MoveAction(VectorOriented( 0.13f,  0.0f,  -PI), true, false, true, true),
+            new MoveAction(VectorOriented( -0.04f,  0.0f,  -PI), false, false, true, true),
+            new StaticAction(OPEN_CLAWS),
+            new MoveAction(VectorOriented( 0.13f,  0.0f,  -PI), false, true, true, true),
+            new StaticAction(LOWER_CLAWS)
+        }
+    );
+    brain_numeriexplore = new SequenceManager({Brain_Numeriexplore});
+    brain_numeriexplore2 = new SequenceManager({Brain_Numeriexplore2}); 
     brain_bleu = new SequenceManager({Brain_Bleu}); 
     brain_jaune = new SequenceManager({Brain_Jaune});
 
@@ -308,7 +331,7 @@ void setup() {
     //robot = new Robot(robotInitBleu.getX(), robotInitBleu.getY(), robotInitBleu.getTheta());
     //robot = new Robot(robotInitJaune.getX(), robotInitJaune.getY(), robotInitJaune.getTheta());
     robot = new Robot(robotInitZero.getX(), robotInitZero.getY(), robotInitZero.getTheta());
-    brain = brain_numeriexplore;
+    brain = brain_numeriexplore2;
     robot->comESP.send(newMessageActuator(Teensy, ESP_32, SetTeamColorBleu));
 
     /* MISC */
